e57c08e3b214951b2e7a1c89b015c0751f8089c4
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-04-02 17:37:48 -0400

    Use single and quint groups too

--- project/Enemies/ObstacleGenerator.gd
+++ project/Enemies/ObstacleGenerator.gd
@@ -3,7 +3,12 @@ extends Spatial
 signal generated(group)
 
 const _Obstacle := preload("res://Enemies/Obstacle.tscn")
-const _Group := preload("res://Enemies/ObstacleGroup.tscn")
+
+const _GROUPS = [
+	preload("res://Enemies/SingleGroup.tscn"),
+	preload("res://Enemies/TripleGroup.tscn"),
+	preload("res://Enemies/QuintupleGroup.tscn"),
+]
 
 export var radius := 20
 export var initial_wait_time := 1.75
@@ -12,6 +17,7 @@ export var time_to_min_wait := 120.0
 export var min_speed := 0.3
 export var max_speed := 0.8
 export var time_to_max_speed := 90.0
+export var time_to_first_quint := 45.0
 
 onready var _timer := $Timer
 
@@ -28,7 +34,7 @@ func _on_Timer_timeout():
 
 
 func _spawn_group()->void:
-	var group : Spatial = _Group.instance()
+	var group : Spatial = _GROUPS[_select_group_index()].instance()
 	var theta := rand_range(0, TAU)
 	var pos2d := Vector2(1,0).rotated(theta) * radius
 	group.translation = Vector3(pos2d.x, 0, pos2d.y)
@@ -38,6 +44,19 @@ func _spawn_group()->void:
 	
 	emit_signal("generated", group)
 
+
+func _select_group_index()->int:
+	if Player.seconds < time_to_first_quint:
+		return 0 if randf() < 0.5 else 1
+	else:
+		var value := randf()
+		if value < 0.2:
+			return 0
+		elif value < 0.6:
+			return 1
+		else:
+			return 2
+	
 
 func _compute_current_speed():
 	return lerp(min_speed, max_speed, min(1.0, Player.seconds/ time_to_max_speed))
