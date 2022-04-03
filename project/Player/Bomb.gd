5a9288d336aae636a5944abcb54161b940caad3e
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-04-02 20:36:32 -0400

    Switch to interlocking toruses

--- project/Player/Bomb.gd
+++ project/Player/Bomb.gd
@@ -12,6 +12,9 @@ enum _State {
 	DEAD
 }
 
+const TORUS_ROTATION_AXES = [Vector3.FORWARD, Vector3.RIGHT]
+const TORUS_ROTATION_SPEEDS = [3,1.66]
+
 export var speed := 10.0
 export var spawn_origin_offset := Vector3(0,50,20)
 
@@ -19,8 +22,8 @@ var _state
 
 onready var _tween := $Tween
 onready var _invincibility_timer := $InvincibilityTimer
-onready var _box := $Box
 onready var _dead_timer := $DeadTimer
+onready var _torii = [$CSGTorus, $CSGTorus2]
 
 func _ready():
 	_set_state(_State.SPAWNING)
@@ -63,10 +66,14 @@ func _set_state(new_state:int)->void:
 func _change_color(color:Color)->void:
 	var material := SpatialMaterial.new()
 	material.albedo_color = color
-	_box.material_override = material
+	for torus in _torii:
+		torus.material_override = material
 
 
-func _physics_process(_delta):
+func _physics_process(delta):
+	for i in _torii.size():
+		_torii[i].rotate(TORUS_ROTATION_AXES[i], TORUS_ROTATION_SPEEDS[i] * delta)
+	
 	if _state == _State.ACTIVE:
 		if Input.is_action_just_pressed("explode"):
 			_state = _State.EXPLODED
