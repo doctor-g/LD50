d40af55c91ebc0ed4c9bfe9598de710b732a7e79
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-04-02 22:15:30 -0400

    Add options menu

--- project/Screens/MainMenu.gd
+++ project/Screens/MainMenu.gd
@@ -1,6 +1,15 @@
 extends Control
 
+onready var _options_dialog := $OptionsDialog
 
 func _on_PlayButton_pressed():
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://World/World.tscn")
+
+
+func _on_OptionsButton_pressed():
+	_options_dialog.show()
+
+
+func _on_OkButton_pressed():
+	_options_dialog.hide()
