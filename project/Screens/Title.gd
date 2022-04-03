cd6023ba462e89073007c1e7c92df8973796bd35
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-04-03 16:40:58 -0400

    Seed the random number generator

--- project/Screens/Title.gd
+++ project/Screens/Title.gd
@@ -1,6 +1,13 @@
 extends Control
 
 
+func _ready():
+	# Doing this here means that when the game scene is run in-engine,
+	# it will be deterministic, but when playing the built version,
+	# it will always be different. Nice.
+	randomize()
+
+
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		$AnimationPlayer.play("fly-up")
